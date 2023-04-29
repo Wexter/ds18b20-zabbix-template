48d2bff178fa114a4dfa7f7c9e4a22717f3c1021
Vitaly Agapov <wesallenalex@gmail.com>
2023-04-29 09:35:35 +0300

    Update discover_temp_sensors.sh

--- discover_temp_sensors.sh
+++ discover_temp_sensors.sh
@@ -9,13 +9,10 @@ echo "\"data\":["
 
 while IFS= read -r line
 do
-  #echo $line
   if (( "$first" != "1" ))
   then
     echo ,
   fi
-  #index=$(echo -n $line | cut -d ":" -f 1 | cut -d " " -f 2)
-  #gpuuuid=$(echo -n $line | cut -d ":" -f 3 | tr -d ")" | tr -d " ")
   echo -n {"\"{#SENSORID}"\":"\"$line"\"}
   if (( "$first" == "1" ))
   then
